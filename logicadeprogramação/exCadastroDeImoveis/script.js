65b11b9cea84792f68e52617eee873de9176da2f
Pedro dos Santos <122922623+pedrosantosgithub@users.noreply.github.com>
2023-09-05 07:34:41 -0300

    Uptade de garagem,alert de volta ao menu, confirmação.

--- logicadeprogramação/exCadastroDeImoveis/script.js
+++ logicadeprogramação/exCadastroDeImoveis/script.js
@@ -1,47 +1,74 @@
 let menu = "";
 let listaImoveis = [];
-let imovel = {};
 
-alert('Bem-Vindo ao Cadastro de Imóveis');
+alert("Bem-Vindo ao Cadastro de Imóveis");
 
 do {
-    menu = prompt(`${listaImóveis.length} imóveis cadastrados\nEscolha uma das opções abaixo:\n
-        a) Adicionar Imóvel\n
-        b) Mostrar imóveis cadastrados\n
-        c) Sair`
-    );
+  menu = prompt(
+    `${listaImoveis.length} imóveis cadastrados\nEscolha uma das opções abaixo:\n
+a) Adicionar Imóvel
+b) Mostrar imóveis cadastrados
+c) Sair`
+  );
 
-    switch(menu) {
-        case "a":
-            let prop = prompt('Qual é o Proprietário');
-            let quart = prompt('Quantos quartos possui?'); // Não redeclare quart
-            let banh = prompt('Quantos Banheiros?'); // Não redeclare banh
+  switch (menu) {
+    case "a":
+      let prop = prompt("Qual é o Proprietário");
+      let quart = prompt("Quantos quartos possui?");
+      let banh = prompt("Quantos Banheiros?");
+      let garag = prompt("Possui garagem?");
 
-            if (
-                typeof prop === 'string' && prop.length >= 1 &&
-                !isNaN(quart) && !isNaN(banh)
-            ) {
-                imovel = { proprietario: prop, quarto: quart, banheiro: banh };
-                listaImoveis.push(imovel);
-                console.log(listaImoveis);
-                alert('Imóvel cadastrado');
-            } else {
-                alert('[ERROR] As caixas estão vazias ou contêm valores incorretos!');
-            }
-            break;
+      const confirmação = confirm(`
+                \nSalvar este imóvel?\n
+                Proprietário: ${prop}\n
+                Quartos: ${quart}\n
+                Banheiros: ${banh}\n
+                Tem garagem?: ${garag}`);
 
-        case "b":
-            listaImoveis.forEach((item) => {
-                console.log(item.proprietario, item.quarto, item.banheiro);
-                alert(`Proprietário: ${item.proprietario}\nQuartos: ${item.quarto}\nBanheiros: ${item.banheiro}`);
-            });
-            break;
+      if (confirmação) {
+        if (
+          typeof prop === "string" &&
+          prop.length >= 1 &&
+          !isNaN(quart) &&
+          !isNaN(banh)
+        ) {
+          const imovel = {
+            proprietario: prop,
+            quarto: quart,
+            banheiro: banh,
+            garagem: garag,
+          };
+          listaImoveis.push(imovel);
+          console.log(listaImoveis);
+          alert("Imóvel cadastrado");
+        } else {
+          alert("[ERROR] As caixas estão vazias ou contêm valores incorretos!");
+        }
+      } else if (confirmação === false) {
+        alert("Você está voltando para o menu!");
+      }
+      break;
 
-        case "c":
-          alert('Encerrando programa....')
-            break;
+    case "b":
+      listaImoveis.forEach((item) => {
+        console.log(
+          item.proprietario,
+          item.quarto,
+          item.banheiro,
+          item.garagem
+        );
+        alert(`Proprietário: ${item.proprietario}\n
+                Quartos: ${item.quarto}\n
+                Banheiros: ${item.banheiro}\n
+                Possui garagem?: ${item.garagem}`);
+      });
+      break;
 
-        default:
-            alert('Opção Inválida');
-    }
-} while(menu !== "c");
+    case "c":
+      alert("Fechando o programa");
+      break;
+
+    default:
+      alert("Opção Inválida");
+  }
+} while (menu !== "c");
